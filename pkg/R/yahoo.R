cdb13d6a5f213cfa0df59b84794965d5e12834e9
popivan <popivan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-04-15 04:15:48 +0000

    Changed the table schema for Yahoo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/btutils@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/yahoo.R
+++ pkg/R/yahoo.R
@@ -7,19 +7,19 @@ YahooDb = R6Class("YahooDb",
       initialize = function(path="yahoo.sqlite") {
          private$path = path
       },
-      
+
       get.symbol = function(symbol, force=F) {
          require(RSQLite)
-         
+
          driver = SQLite()
          connection = dbConnect(driver, dbname=private$path)
-         
+
          symbol = toupper(symbol)
-         
+
          # Leading '^' (used for indexes, like ^DJI) is dropped when
          # quantmod constructs the variable name
          db.symbol = gsub('^\\^', '', symbol)
-         
+
          if(!force) {
             # Try the database first
             rs = dbGetQuery(
@@ -38,7 +38,7 @@ YahooDb = R6Class("YahooDb",
          } else {
             rs = NULL
          }
-         
+
          if(NROW(rs) == 0) {
             ss = getSymbols(symbol, from="1900-01-01", auto.assign=F)
             ss = adjustOHLC(ss, use.Adjusted=F, adjust="split", symbol.name=symbol)
@@ -52,7 +52,7 @@ YahooDb = R6Class("YahooDb",
                           "   values(@symbol,@date,@open,@high,@low,@close,@volume,@adjusted)",
                           sep="")
             RSQLite::dbGetPreparedQuery(connection, query, bind.data=df)
-            
+
             if(db.symbol != symbol) {
                # If the database symbol (GSPC) is different than the yahoo symbol (^GSPC),
                # write the mapping.
@@ -61,28 +61,28 @@ YahooDb = R6Class("YahooDb",
                              sep="")
                RSQLite::dbGetPreparedQuery(connection, query, bind.data=data.frame(symbol=db.symbol,yahoo_symbol=symbol))
             }
-            
+
             dbCommit(connection)
          }
-         
-         
+
+
          dbDisconnect(connection)
-         
+
          return(ss)
       },
-      
+
       get.symbols = function(symbols, env, force=F) {
          require(RSQLite)
-         
+
          driver = SQLite()
          connection = dbConnect(driver, dbname=private$path)
-         
+
          symbols = toupper(symbols)
-         
+
          # Leading '^' (used for indexes, like ^DJI) is dropped when
          # quantmod constructs the variable name
          db.names = gsub('^\\^', '', symbols)
-         
+
          done = FALSE
          if(!force) {
             # Try the local cache first
@@ -102,13 +102,13 @@ YahooDb = R6Class("YahooDb",
                   colnames(env[[ss]]) = private$col.names
                }
             }
-            
+
             # Verify that we got all symbols, in which case we are done
             if(!any(is.na(match(db.names, ls(env))))) {
                done = TRUE
             }
          }
-         
+
          if(!done) {
             # Either a symbol was not found in the cache, or the caller forced us to download and cache
             getSymbols(symbols, env=env, src="yahoo", from="1900-01-01", auto.assign=TRUE)
@@ -117,11 +117,11 @@ YahooDb = R6Class("YahooDb",
                # Adjust only for splits
                env[[ss]] = adjustOHLC(env[[ss]], use.Adjusted=F, adjust="split", symbol.name=symbols[as.numeric(match(ss, db.names))])
                colnames(env[[ss]]) = private$col.names
-               
+
                # Store into the database
                df = cbind(data.frame(symbol=ss), as.character(index(env[[ss]])), data.frame(env[[ss]]))
                colnames(df) = c("symbol","date",private$col.names)
-               
+
                query = paste("delete from bars where symbol='", ss, "'", sep="")
                dbSendQuery(connection, query)
                query = paste(" insert or replace into bars (symbol,date,open,high,low,close,volume,adjusted) ",
@@ -129,7 +129,7 @@ YahooDb = R6Class("YahooDb",
                              sep="")
                RSQLite::dbGetPreparedQuery(connection, query, bind.data=df)
             }
-            
+
             # Write all mappings
             for(ii in 1:NROW(symbols)) {
                if(db.names[ii] != symbols[ii]) {
@@ -144,10 +144,10 @@ YahooDb = R6Class("YahooDb",
                      bind.data=data.frame(symbol=db.names[ii],yahoo_symbol=symbols[ii]))
                }
             }
-            
+
             dbCommit(connection)
          }
-         
+
          dbDisconnect(connection)
       },
 
@@ -155,18 +155,18 @@ YahooDb = R6Class("YahooDb",
       g = function(symbol, force=F) {
          return(self$get.symbol(symbol, force))
       },
-      
-      
+
+
       gs = function(symbols, env, force=F) {
          return(self$get.symbols(symbols, env, force))
       },
-      
+
       store.symbol = function(symbol, data) {
          require(RSQLite)
-         
+
          driver = SQLite()
          connection = dbConnect(driver, dbname=private$path)
-         
+
          df = cbind(data.frame(symbol=symbol), as.character(index(data)), data.frame(data))
          colnames(df) = c("symbol","date",private$col.names)
          dbBegin(connection)
@@ -179,13 +179,13 @@ YahooDb = R6Class("YahooDb",
          dbCommit(connection)
          dbDisconnect(connection)
       },
-      
+
       init = function() {
          require(RSQLite)
-         
+
          driver = SQLite()
          connection = dbConnect(driver, dbname=private$path)
-         
+
          query = paste(" create table if not exists bars ( ",
                        " symbol varchar(30) not null, ",
                        " date datetime not null, ",
@@ -193,7 +193,7 @@ YahooDb = R6Class("YahooDb",
                        " high real not null, ",
                        " low real not null, ",
                        " close real not null, ",
-                       " volume integer not null, ",
+                       " volume real not null, ",
                        " adjusted real not null) ",
                        sep="")
          dbGetQuery(connection, query)
@@ -201,7 +201,7 @@ YahooDb = R6Class("YahooDb",
                        "    on bars(symbol,date) ",
                        sep="")
          dbGetQuery(connection, query)
-         
+
          query = paste(" create table if not exists map ( ",
                        " symbol varchar(30) not null, ",
                        " yahoo_symbol varchar(30) not null) ",
@@ -211,11 +211,11 @@ YahooDb = R6Class("YahooDb",
                        "    on map(symbol) ",
                        sep="")
          dbGetQuery(connection, query)
-         
+
          dbDisconnect(connection)
       }
    ),
-   
+
    private = list(
       path = "yahoo.sqlite",
       col.names = c("open","high","low","close","volume","adjusted")
